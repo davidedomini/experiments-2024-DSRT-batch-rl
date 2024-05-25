15436eb5e622f687db333e85f0963b543e7ec541
davidedomini <davide.domini@unibo.it>
2024-05-25 13:55:36 +0200

    feat: implement async execution of simulations

--- src/main/scala/it/unibo/alchemist/boundary/launchers/LearningLauncher.scala
+++ src/main/scala/it/unibo/alchemist/boundary/launchers/LearningLauncher.scala
@@ -1,18 +1,19 @@
 package it.unibo.alchemist.boundary.launchers
 
 import com.google.common.collect.Lists
-import it.unibo.alchemist.boundary.launchers.LearningLauncher.logger
 import it.unibo.alchemist.boundary.{Launcher, Loader, Variable}
 import it.unibo.alchemist.core.Simulation
 import it.unibo.alchemist.model.molecules.SimpleMolecule
 import it.unibo.alchemist.util.BugReporting
 import org.slf4j.{Logger, LoggerFactory}
-
 import scala.jdk.CollectionConverters._
-import java.util.concurrent.{ConcurrentLinkedQueue, Executors}
-import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.{ConcurrentLinkedQueue, Executors, TimeUnit}
 import scala.collection.mutable
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContextExecutor}
+import scala.concurrent.{ExecutionContext, Future}
 import scala.jdk.CollectionConverters.IteratorHasAsScala
+import scala.util.{Failure, Success}
 
 class LearningLauncher (
                          val batch: java.util.ArrayList[String],
@@ -23,26 +24,27 @@ class LearningLauncher (
 
   private val parallelism: Int = Runtime.getRuntime.availableProcessors()
   private val logger: Logger = LoggerFactory.getLogger(this.getClass.getName)
+  private val errorQueue = new ConcurrentLinkedQueue[Throwable]()
+  private val executor = Executors.newFixedThreadPool(parallelism)
+  private implicit val executionContext: ExecutionContextExecutor = ExecutionContext.fromExecutor(executor)
 
   override def launch(loader: Loader): Unit = {
     val instances = loader.getVariables
     val prod = cartesianProduct(instances, batch)
 
-    val workerId = new AtomicInteger(0)
-    val errorQueue = new ConcurrentLinkedQueue[Throwable]()
-
     Range.inclusive(1, globalRounds).foreach { iter =>
-      logger.info(s"---------------- Global Round $iter ----------------")
-      prod.zipWithIndex.foreach {
+      logger.info(s"Starting Global Round: $iter")
+      val futures = prod.zipWithIndex.map {
         case (instance, index) =>
           val sim = loader.getWith[Any, Nothing](instance.asJava)
-          neuralNetworkInjection(sim)
-          sim.play()
-          sim.run()
-          logger.info("Simulation with {} completed successfully", instance)
+          runSimulationAsync(sim, index, instance)
       }
+      Await.ready(Future.sequence(futures), Duration.Inf)
     }
 
+    executor.shutdown()
+    executor.awaitTermination(Long.MaxValue, TimeUnit.DAYS)
+    // TODO - throws errors in errorQueue
   }
 
   private def cartesianProduct(
@@ -61,15 +63,37 @@ class LearningLauncher (
       .asInstanceOf[List[mutable.Map[String, Serializable]]]
   }
 
- private def neuralNetworkInjection(simulation: Simulation[Any, Nothing]): Unit = {
-   simulation
-     .getEnvironment
-     .getNodes
-     .iterator()
-     .asScala.toList
-     .foreach { node =>
-       node.setConcentration(new SimpleMolecule("Model"), 2) // TODO - set real model with scalapy
-     }
- }
+  private def runSimulationAsync(
+    simulation: Simulation[Any, Nothing],
+    index: Int,
+    instance: mutable.Map[String, Serializable]
+  )(implicit executionContext: ExecutionContext): Future[Unit] = {
+    val future = Future {
+      neuralNetworkInjection(simulation)
+      simulation.play()
+      simulation.run()
+      simulation.getError.ifPresent { error => throw error}
+      logger.info("Simulation with {} completed successfully", instance)
+    }
+    future.onComplete {
+      case Success(_) => logger.info("Simulation {} of {} completed", index + 1, instance.size)
+      case Failure(exception) =>
+        logger.error(s"Failure for simulation with $instance", exception)
+        errorQueue.add(exception)
+        executor.shutdownNow()
+    }
+    future
+  }
+
+  private def neuralNetworkInjection(simulation: Simulation[Any, Nothing]): Unit = {
+    simulation
+      .getEnvironment
+      .getNodes
+      .iterator()
+      .asScala.toList
+      .foreach { node =>
+         node.setConcentration(new SimpleMolecule("Model"), 2) // TODO - set real model with scalapy
+      }
+  }
 
 }
