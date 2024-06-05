2b1816bf4892944bf3d39c7c14113547c0f4ada8
Davide Domini <davide.domini@unibo.it>
2024-06-05 15:17:52 +0200

    feat: implement experience collection reaction

---
+++ src/main/scala/it/unibo/experiment/ExperienceCollectionReaction.scala
@@ -0,0 +1,28 @@
+package it.unibo.experiment
+
+import it.unibo.alchemist.model.implementations.reactions.AbstractGlobalReaction
+import it.unibo.alchemist.model.learning.{Experience, ExperienceBuffer, Molecules, State}
+import it.unibo.alchemist.model.molecules.SimpleMolecule
+import it.unibo.alchemist.model.{Environment, Position, TimeDistribution}
+import it.unibo.experiment.FlockState.encoder
+import it.unibo.experiment.ActionSpace.encoder
+
+class ExperienceCollectionReaction [T, P <: Position[P]] (
+    environment: Environment[T, P],
+    distribution: TimeDistribution[T]
+  ) extends AbstractGlobalReaction(environment, distribution) {
+
+  override protected def executeBeforeUpdateDistribution(): Unit = {
+    nodes.foreach { node =>
+      val actualState = node.getConcentration(new SimpleMolecule(Molecules.actualState)).asInstanceOf[FlockState]
+      val nextState = node.getConcentration(new SimpleMolecule(Molecules.nextState)).asInstanceOf[FlockState]
+      val reward = node.getConcentration(new SimpleMolecule(Molecules.reward)).asInstanceOf[Double]
+      val actionID = node.getConcentration(new SimpleMolecule(Molecules.action)).asInstanceOf[Int]
+      val action = ActionSpace.all(actionID)
+      val experience = node.getConcentration(new SimpleMolecule(Molecules.experience)).asInstanceOf[ExperienceBuffer[FlockState]]
+      experience.insert(Experience(actualState, action, reward, nextState))
+      node.setConcentration(new SimpleMolecule(Molecules.experience), experience.asInstanceOf[T])
+    }
+  }
+ 
+}
