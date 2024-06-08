52e1e88ea707c676161ed70d950c5e346d72cd09
davidedomini <davide.domini@unibo.it>
2024-06-08 10:56:38 +0200

    fix: fix generics

--- src/main/scala/it/unibo/alchemist/model/layers/ModelLayer.scala
+++ src/main/scala/it/unibo/alchemist/model/layers/ModelLayer.scala
@@ -3,11 +3,11 @@ package it.unibo.alchemist.model.layers
 import it.unibo.alchemist.model.{Environment, Layer, Position}
 import me.shadaj.scalapy.py
 
-class ModelLayer[P <: Position[P]](
-    environment: Environment[_, P],
+class ModelLayer[T, P <: Position[P]](
+    environment: Environment[T, P],
     private val model: py.Dynamic
-  ) extends Layer[py.Dynamic, P] {
-  override def getValue(p: P): py.Dynamic = {
-    model
+  ) extends Layer[T, P] {
+  override def getValue(p: P): T = {
+    model.asInstanceOf[T]
   }
 }
